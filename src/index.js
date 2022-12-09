a74a8f4ba6a70d650453d1afbd7cca7f9ba3aef4
Robert Leung <rleung84@uwo.ca>
2022-12-08 21:34:11 -0500

    Updates to openweather API calls and response handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import "./style.css";
 
+const apiKey = "3650680fe878433ad9588e6595f3c6f1";
 const content = document.querySelector("#content");
-const h1 = document.createElement("h1");
-h1.innerText = "Welcome";
-content.appendChild(h1);
+const temp = document.createElement("div");
+let location;
+
+const successCallback = (position) => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const locationURL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
+  fetch(
+    locationURL,
+    { mode: "cors" },
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      location = response[0].name;
+    });
+};
+
+const errorCallback = (error) => {
+  console.log(error);
+  location = "Ottawa";
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}&units=metric`;
+
+fetch(
+  url,
+  { mode: "cors" },
+)
+  .then((response) => console.log(response.json()));
+// .then((response) => {
+//   temp.innerText = response.main.temp;
+// });
+const greeting = document.createElement("h1");
+greeting.innerText = "Welcome";
+content.appendChild(temp);
+content.appendChild(greeting);
